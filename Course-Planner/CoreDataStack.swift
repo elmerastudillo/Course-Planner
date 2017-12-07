eb5aed60c8e6c7866c60c2fc956a298829c877d9
elmerastudillo <astudilloelmer@icloud.com>
2017-12-06 16:08:39 -0800

    Implemented the Core Data Stack

---
+++ Course-Planner/CoreDataStack.swift
@@ -0,0 +1,53 @@
+//
+//  CoreDataStack.swift
+//  Course-Planner
+//
+//  Created by Elmer Astudillo on 12/6/17.
+//  Copyright © 2017 Elmer Astudillo. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+public final class CoreDataStack
+{
+    static let instance = CoreDataStack()
+    
+    // Create a persistent container
+    private lazy var persistanceContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer (
+            name: "Course-Planner"
+            )
+        container.loadPersistentStores(completionHandler: { (coureDescription, error) in
+            if let error = error
+            {
+                fatalError("Unresolved error \(error), \(String(describing: error._userInfo))")
+            }
+        })
+        return container
+    }()
+    
+    // Access coredata using the main thread
+    lazy var viewContext : NSManagedObjectContext = {
+        let viewContext = persistanceContainer.viewContext
+        return viewContext
+    }()
+    
+    lazy var privateContext : NSManagedObjectContext = {
+        let privateCon = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        privateCon.persistentStoreCoordinator = persistanceContainer.persistentStoreCoordinator
+        return privateCon
+    }()
+    
+    func save(context: NSManagedObjectContext)
+    {
+        if context.hasChanges
+        {
+            
+        }
+    }
+    
+    
+    
+    
+}
